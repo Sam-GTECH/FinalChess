9fdaf3c6d6b6fd0c79abe46eeb24e6b26acf953c
Sam-GTECH <sburton@gaming.tech>
2024-10-14 07:30:02 +0200

    Knight mouvements

--- src/FinalChess/piece_knight.cpp
+++ src/FinalChess/piece_knight.cpp
@@ -8,5 +8,12 @@ Knight::Knight()
 Knight::~Knight() {}
 
 bool Knight::CanMoveTo(Board* board, int x, int y) {
-	return true;
+	int myX = board->getPieceX(this);
+	int myY = board->getPieceY(this);
+
+	if (board->getCase(x, y)->getPiece() != nullptr && board->getCase(x, y)->getPiece()->is_white == is_white)
+		return false;
+
+	return  (abs(myX - x) == 2 && abs(myY - y) == 1) ||
+			(abs(myX - x) == 1 && abs(myY - y) == 2);
 }
